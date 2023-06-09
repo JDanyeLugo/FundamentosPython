f19e0c753d2bea70ea65df9ab2ad4e09b7c65d6a
JDanyeLugo <ldanye50@gmail.com>
2023-06-09 16:20:33 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+#python es No tipado
+
+name = "Danye"
+last_name = "Lugo"
+age = 18
+height = 1.60
+is_student = False
+groups = ["3C","3CD"]
+
+# funcion type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
