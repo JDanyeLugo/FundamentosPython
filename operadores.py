530d786fe258e13791655fee18d0f0581356adb5
JDanyeLugo <ldanye50@gmail.com>
2023-06-09 16:52:02 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,18 @@
+# operadores matematicos
+# + - * %
+#/Divicion float
+#//Divicion entera
+#**potencia
+print(12+12)
+print(12-12)
+print(13*13)
+print(12%12)
+print(10/3)
+print(10//3)
+print(2**16)
+
+# operadores para strings
+# + para concatenar
+# * para replicar
+print("hola"+("mundo"))
+print("hola "*3)
